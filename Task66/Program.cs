68c4a97e2e818e525b1a98e728c960488db8b83c
Vladimir <saintprize@gmail.com>
2023-02-15 21:38:59 +0300

    add homework

---
+++ Task66/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine());
+}
+
+int SumNumbers(int NumberM, int NumberN)
+{
+    if (NumberM > NumberN) 
+    {
+        return 0;
+    }
+    else 
+    {
+        return NumberM + SumNumbers(NumberM + 1, NumberN);
+    }
+}
+
+int NumberM = GetNum("Введите значение M: ");
+int NumberN = GetNum("Введите значение N: ");
+
+int sum = SumNumbers(NumberM, NumberN);
+Console.Write($"M = {NumberM}; N = {NumberN} -> {sum}");
---
+++ Task68/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//M = 4; N = 8. -> 30
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine());
+}
+
+int AkkermanFormula(int NumberM, int NumberN)
+{
+    if (NumberM == 0)
+    {
+        return NumberN + 1;
+    }
+    else if (NumberN == 0)
+    {
+        return AkkermanFormula(NumberM - 1, 1);
+    }
+    else
+    {
+        return AkkermanFormula(NumberM - 1, AkkermanFormula(NumberM, NumberN - 1));
+    }
+}
+
+int NumberM = GetNum("Введите значение M: ");
+int NumberN = GetNum("Введите значение N: ");
+int Result = AkkermanFormula(NumberM, NumberN);
+Console.WriteLine($"A({NumberM},{NumberN}) = {Result}");
